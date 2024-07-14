0a7faad2e1caebae1ccac00450080bb4bd6f96ca
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-14 14:57:33 +0300

    completed my algorithms palindrme lab

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //split the string into an array
+  let charArray = word.split('');
+  //reverse the array of characters
+  let reversedArray = charArray.reverse();
+  //join the array back to string after reversing
+  let reversedStr = reversedArray.join('');
+  //compare the reversed str with tyhe initial word
+  return word === reversedStr;
 }
 
 /* 
   Add your pseudocode here
+  create a func isPalindrome with a string arg
+  split the string into an array to be able to reverse it
+  reverse the cgharac ters of the array
+  join the reversed characters of the array into a string
+  compare the initial string with the new string.
+  if they match, its true. if tghey don't, its false
 */
 
 /*
   Add written explanation of your solution here
+  The `isPalindrome` function checks if a given string reads the same forwards and backwards.
+  - It first splits the input string into an array of characters.
+  - Then, it reverses this array.
+  - Next, it joins the reversed array back into a string.
+  - Finally, it compares the original string with the reversed string. If they are equal, it returns `true`, indicating that the string is a palindrome. If they are not equal, it returns `false`.
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +40,17 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("expecting: true");
+  console.log("=>", isPalindrome("level"));
+
+  console.log ("expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("expecting: false");
+  console.log("=>", isPalindrome("mother"));
+  
+
 }
 
 module.exports = isPalindrome;
